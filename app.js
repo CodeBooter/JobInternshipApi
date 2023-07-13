6865222e33b044da794ad3c136fe139b68abff7b
Manav Khadka <96826294+Manav-khadka@users.noreply.github.com>
2023-07-13 18:48:57 +0000

    minor bug fix on route

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cookieParser = require("cookie-parser");
 const jobRoute = require('./routes/job_route');
 const stipendRoute = require('./routes/stipend_route');
-const DSAroute=require('./routes/DSA_routes');
+// const DSAroute=require('./routes/DSA_routes');
 
 //const userRoute=require('./routes/user_route');
 const jobScheduler = require('./scheduler/jobScheduler');
@@ -17,7 +17,7 @@ app.use(cookieParser());
 
 app.use('/api/v1', jobRoute);
 app.use("/api/v1", stipendRoute);
-app.use("api/v1",DSAroute);
+// app.use("api/v1",DSAroute);
 
 //app.use("/api/v1",userRoute);
 
